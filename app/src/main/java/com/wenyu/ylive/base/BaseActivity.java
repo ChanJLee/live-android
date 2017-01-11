66a28d997178b7fa760475403f6267aa5fb9b3cf
jiacheng <jiacheng.li@shanbay.com>
2017-01-11 22:04:14 +0800

    clean code

---
+++ app/src/main/java/com/wenyu/ylive/base/BaseActivity.java
@@ -0,0 +1,11 @@
+package com.wenyu.ylive.base;
+
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by jiacheng.li on 17/1/7.
+ * Copyright © 2016年 扇贝网(shanbay.com).
+ * All rights reserved.
+ */
+public class BaseActivity extends AppCompatActivity {
+}
