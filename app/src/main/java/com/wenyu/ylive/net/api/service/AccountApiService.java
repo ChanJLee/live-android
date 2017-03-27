19fefdd94ae2ed8ad0866a41a58d283baa1b5fa4
jiacheng <jiacheng.li@shanbay.com>
2017-03-27 23:13:27 +0800

    fix gradle

--- app/src/main/java/com/wenyu/ylive/net/api/service/AccountApiService.java
+++ app/src/main/java/com/wenyu/ylive/net/api/service/AccountApiService.java
@@ -4,6 +4,7 @@ import android.content.Context;
 import android.support.annotation.NonNull;
 
 import com.google.gson.JsonElement;
+import com.wenyu.network.BaseApiService;
 import com.wenyu.ylive.BuildConfig;
 import com.wenyu.ylive.net.api.AccountApi;
 import com.wenyu.ylive.net.model.User;
@@ -16,35 +17,35 @@ import rx.Observable;
  * Copyright © 2016年 扇贝网(shanbay.com).
  * All rights reserved.
  */
-public class AccountApiService {
+public class AccountApiService extends BaseApiService<AccountApi> {
 
 	private static AccountApiService sAccountApiService;
-//
-//	public AccountApiService(Context context, String domain, @NonNull Class<AccountApi> accountApiClass) {
-//		super(context, domain, accountApiClass);
-//	}
+
+	public AccountApiService(Context context, String domain, @NonNull Class<AccountApi> accountApiClass) {
+		super(context, domain, accountApiClass);
+	}
 
 	public static AccountApiService getAccountApiService() {
 		if (sAccountApiService == null) {
 			synchronized (AccountApiService.class) {
 				if (sAccountApiService == null) {
-	//				sAccountApiService = new AccountApiService(AppClient.getAppContext(), BuildConfig.BASE_URI, AccountApi.class);
+					sAccountApiService = new AccountApiService(AppClient.getAppContext(), BuildConfig.HTTP_BASE_URI, AccountApi.class);
 				}
 			}
 		}
 
 		return sAccountApiService;
 	}
-//
-//	public Observable<User> login(String username, String password) {
-//		return convert(getAPI().login(username, password));
-//	}
-//
-//	public Observable<JsonElement> register(String username, String password, String email) {
-//		return convert(getAPI().register(username, password, email));
-//	}
-//
-//	public Observable<JsonElement> logout() {
-//		return convert(getAPI().logout());
-//	}
+
+	public Observable<User> login(String username, String password) {
+		return convert(getAPI().login(username, password));
+	}
+
+	public Observable<JsonElement> register(String username, String password, String email) {
+		return convert(getAPI().register(username, password, email));
+	}
+
+	public Observable<JsonElement> logout() {
+		return convert(getAPI().logout());
+	}
 }
