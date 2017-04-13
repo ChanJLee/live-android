28afb1c708565a1efe40e2adb83967063bcfc502
jiacheng <jiacheng.li@shanbay.com>
2017-04-13 22:49:25 +0800

    add video

---
+++ app/src/main/java/com/wenyu/ylive/biz/live/LiveActivity.java
@@ -0,0 +1,15 @@
+package com.wenyu.ylive.biz.live;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.wenyu.ylive.R;
+
+public class LiveActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_live);
+    }
+}
---
+++ app/src/main/java/com/wenyu/ylive/biz/video/VideoActivity.java
@@ -0,0 +1,15 @@
+package com.wenyu.ylive.biz.video;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.wenyu.ylive.R;
+
+public class VideoActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_video);
+    }
+}
